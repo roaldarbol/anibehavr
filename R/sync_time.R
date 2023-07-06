9cef849c0291d9a40d5171f24ae1902c37ec6888
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2023-07-06 15:34:40 +0100

    join_timeseries now handles a single group and has test for that

--- R/sync_time.R
+++ R/sync_time.R
@@ -22,33 +22,71 @@ sync_time <- function(.data, data2, x, y) {
       {{ y }} := y)
 }
 
+#' @export
+#' @rdname join_timeseries
 join_timeseries <- function(
     .x,
     y,
     by = NULL,
+    group = NULL,
     method = NA,
     copy = FALSE,
-    suffix = c(".x", ".y"),
-    ...,
+    suffix = c("", ".y"),
     keep = NULL) {
 
-  by_x <- .x[[{{ by }}]]
-  by_y <- y[[{{ by }}]]
-  by_both <- c(by_x, by_y) %>%
-    as_tibble() %>%
-    arrange(value) %>%
-    distinct() %>%
-    rename(
-      {{ by }} := value
-    )
+  # Start out by creating all the grouping - data should have all the relevant times for each group.
+  tbl_group_vars <- group_vars(.x)
 
-  output_tibble <- by_both %>%
-    left_join(.x, by = {{ by }}) %>%
-    left_join(y, by = {{ by }}, suffix = {{ suffix }})
-
-  if (!is.na({{ method }})){
-
+  if (length({{ tbl_group_vars }}) > 0){
+    grouped_x <- .x %>%
+      group_split()
+    grouped_y <- y %>%
+      group_split()
+  } else {
+    grouped_x <- list(.x)
+    grouped_y <- list(y)
   }
 
+  output_list <- list()
+  for (i in 1:length(grouped_x)){
+    current_x <- grouped_x[[i]]
+    current_y <- grouped_y[[i]]
+    by_x <- current_x[[{{ by }}]]
+    by_y <- current_y[[{{ by }}]]
+    by_both <- c(by_x, by_y) %>%
+      as_tibble() %>%
+      arrange(value) %>%
+      distinct() %>%
+      rename(
+        {{ by }} := value
+      )
+
+    current_combined <- by_both %>%
+      left_join(current_x, by = {{ by }}) %>%
+      left_join(current_y, by = {{ by }}, suffix = {{ suffix }})
+
+    # Finish grouping
+    if (length({{ tbl_group_vars }}) > 0){
+      # Find unique grouping level
+      group_level <- current_combined %>%
+        select({{ tbl_group_vars }}) %>%
+        distinct() %>%
+        na.omit() %>%
+        pull()
+
+      # Replace NAs in grouping column
+      current_combined <- current_combined %>%
+        mutate({{ tbl_group_vars }} := {{ group_level }})
+
+
+      # Remove extra group column
+      current_combined <- current_combined %>%
+        select(-paste0({{ tbl_group_vars }}, ".y"))
+    }
+
+    output_list[[i]] <- current_combined
+  }
+
+  output_tibble <- bind_rows(output_list)
   return(output_tibble)
 }
--- tests/testthat/test-sync_time.R
+++ tests/testthat/test-sync_time.R
@@ -4,16 +4,35 @@ test_that("Test join_timeseries to gather two unsyncronised data frames", {
                  value = seq(1, 3, 1))
   df_b <- tibble(time = seq(0.5, 2.5, 0.5),
                  other_value = seq(-5, -4, 0.25))
+  df_id_single <- tibble(
+    time = c(1,2,3,1,2,3),
+    id = c(1,1,1,2,2,2),
+    value = c(1,2,3,4,5,6)
+    ) %>%
+    group_by(id)
+  df_id_single_2 <- tibble(
+    time = c(1,2,3,4,2,3),
+    id = c(1,1,1,1,2,2),
+    value2 = c(1,2,3,4,5,6)
+    ) %>%
+    group_by(id)
 
   # Run the default function with non-conflicting column names
   df_test_default <- df_a %>%
     join_timeseries(
       y = df_b,
       by = "time")
+
   # With conflicting column names, default suffix
   df_test_default_suffix <- df_a %>%
     join_timeseries(
       y = df_a,
+      by = "time")
+
+  # With a single group
+  df_test_single_group <- df_id_single %>%
+    join_timeseries(
+      y = df_id_single_2,
       by = "time")
 
   # Expected output (made with `constructive::construct()`)
@@ -25,10 +44,18 @@ test_that("Test join_timeseries to gather two unsyncronised data frames", {
 
   expected_default_suffix <- tibble(
     time = c(0, 1, 2),
-    value.x = c(1, 2, 3),
+    value = c(1, 2, 3),
     value.y = c(1, 2, 3)
     )
 
+  expected_single_group <- tibble::tibble(
+    time = c(1, 2, 3, 4, 1, 2, 3),
+    id = rep(c(1, 2), 4:3),
+    value = c(1, 2, 3, NA, 4, 5, 6),
+    value2 = c(1, 2, 3, 4, NA, 5, 6),
+  )
+
   expect_equal(df_test_default, expected_default)
   expect_equal(df_test_default_suffix, expected_default_suffix)
+  expect_equal(df_test_single_group, expected_single_group)
 })
