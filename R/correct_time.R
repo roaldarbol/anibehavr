84f5229ec109dbd0ab2d74a60075a2bcea18a713
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2023-12-21 19:09:48 +0100

    improve performance of correct_time with data.table

--- R/correct_time.R
+++ R/correct_time.R
@@ -2,23 +2,19 @@
 #'
 #' @param data
 #'
-#' @return
+#' @import data.table
+#' @return A data frame where times are added.
 #' @export
 #'
 correct_time <- function(data) {
   n_vids <- unique(na.omit(data$vid))
+  data.table::setDT(data)
   if (length(n_vids) >= 2){
     for (i in 2:length(n_vids)) {
-      max_time_vid <- data %>%
-        filter(.data$vid == i-1) %>%
-        summarise(max_time_vid = max(.data$time, na.rm = TRUE)) %>%
-        as.numeric()
-
-      data <- data %>%
-        mutate(time = if_else(.data$vid == i,
-                              .data$time + max_time_vid,
-                              .data$time))
+      max_time_vid <- data[which(vid==i-1), max(time)]
+      data[which(vid==i), time := time + max_time_vid]
     }
   }
+  data.table::setDF(data)
   return(data)
 }
