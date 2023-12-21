83dc868708efb7b7f70a8e4510cda5a080db0014
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2023-12-21 19:45:32 +0100

    maybe it worked after all

--- R/correct_time.R
+++ R/correct_time.R
@@ -8,6 +8,19 @@
 #'
 correct_time <- function(data) {
   n_vids <- unique(na.omit(data$vid))
+  data.table::setDT(data)
+  if (length(n_vids) >= 2){
+    for (i in 2:length(n_vids)) {
+      max_time_vid <- data[which(vid==i-1), max(time)]
+      data[which(vid==i), time := time + max_time_vid]
+    }
+  }
+  data.table::setDF(data)
+  return(data)
+}
+
+correct_time2 <- function(data) {
+  n_vids <- unique(na.omit(data$vid))
   if (length(n_vids) >= 2){
     for (i in 2:length(n_vids)) {
       max_time_vid <- data %>%
