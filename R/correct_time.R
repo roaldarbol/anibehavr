1582250b4bc8bef705715d65e2d048b23d164b8d
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2023-12-21 19:32:33 +0100

    correct_time broke with data.table - reverting

--- R/correct_time.R
+++ R/correct_time.R
@@ -8,6 +8,25 @@
 #'
 correct_time <- function(data) {
   n_vids <- unique(na.omit(data$vid))
+  if (length(n_vids) >= 2){
+    for (i in 2:length(n_vids)) {
+      max_time_vid <- data %>%
+        filter(.data$vid == i-1) %>%
+        summarise(max_time_vid = max(.data$time, na.rm = TRUE)) %>%
+        as.numeric()
+
+      data <- data %>%
+        mutate(time = if_else(.data$vid == i,
+                              .data$time + max_time_vid,
+                              .data$time))
+    }
+  }
+  return(data)
+}
+
+
+correct_time2 <- function(data) {
+  n_vids <- unique(na.omit(data$vid))
   data.table::setDT(data)
   if (length(n_vids) >= 2){
     for (i in 2:length(n_vids)) {
