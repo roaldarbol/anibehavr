c9a76044cfb1915f192d365bdba4707046c46e62
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2024-03-18 17:22:58 +0000

    Classify multiple states at the same time

--- R/classify_states.R
+++ R/classify_states.R
@@ -15,29 +15,44 @@
 classify_states <- function(
     data,
     movement_var,
-    window_width,
+    window_widths,
     .keep = FALSE){
 
-  data <- data |>
-    group_modify(
-      ~ filter_forward_backward(
-        .x,
-        movement_var = is_locomoting,
-        window_width = 10)
-    ) |>
-    dplyr::mutate(
-      rolling_right = dplyr::if_else(rolling_right > 0, 1, 0),
-      rolling_left = dplyr::if_else(rolling_left > 0, 1, 0),
-      state = dplyr::if_else(rolling_right > 0 & rolling_left > 0, 1, 0),
-      state_change = dplyr::if_else(state != dplyr::lag(state), 1, 0),
-      state_change = dplyr::if_else(is.na(state_change), 0, state_change),
-      state_number = cumsum(state_change)
-    )
+  for (i in 1:length(window_widths)){
+
+    # Generate dynamic names
+    n <- as.numeric(i)
+    rolling_right_new <- sym(paste("rolling_right", n, sep = "_"))
+    rolling_left_new <- sym(paste("rolling_left", n, sep = "_"))
+    state <- sym(paste("state", n, sep = "_"))
+    state_change <- sym(paste("state_change", n, sep = "_"))
+    state_number <- sym(paste("state_number", n, sep = "_"))
+
+    new_cols <- data |>
+      group_modify(
+        ~ filter_forward_backward(
+          .x,
+          movement_var = is_locomoting,
+          window_width = 10)
+      ) |>
+      dplyr::mutate(
+        "{{ rolling_right_new }}" := dplyr::if_else(rolling_right > 0, 1, 0),
+        "{{ rolling_left_new }}" := dplyr::if_else(rolling_left > 0, 1, 0),
+        "{{ state }}" := dplyr::if_else({{ rolling_right_new }} > 0 & {{ rolling_left_new }} > 0, 1, 0),
+        "{{ state_change }}" := dplyr::if_else({{ state }} != dplyr::lag({{ state }}), 1, 0),
+        "{{ state_change }}" := dplyr::if_else(is.na({{ state_change }}), 0, {{ state_change }}),
+        "{{ state_number }}" := cumsum({{ state_change }})
+      ) |>
+      ungroup() |>
+      select(last_col(4):last_col())
+    data <- bind_cols(data, new_cols)
+  }
+
 
   # Get rid of the filters themselves
   if (.keep == FALSE){
     data <- data |>
-      select(-rolling_right, -rolling_left)
+      select(-starts_with("rolling_"))
   }
   return(data)
 }
